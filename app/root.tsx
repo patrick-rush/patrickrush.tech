b18566d23698d3f04e18ce1d063c12c7fce27390
patrick-rush <patrick.a.rush@gmail.com>
2023-11-27 18:57:17 -0600

    Add basic stack and font styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+// import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -9,20 +10,35 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import Nav from "~/components/nav"
+import { getUser } from "~/session.server";
+import stylesheet from "~/tailwind.css";
+
+// import "@fontsource-variable/open-sans/wght.css";
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous"},
+  { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Cutive+Mono&family=Rubik+Mono+One&display=swap" }
+  // ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return json({ user: await getUser(request) });
+};
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
+        <Nav />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cutive: ['"Cutive Mono"', ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
